01f87ceb1cefeabcccfb4f669735c3b259395a7d
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-08-04 00:01:11 +0400

    Added setup files + accordion component

---
+++ src/components/Accordion.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+const Accordion = ({ items }) => {
+    return (
+        <div>We have {items.length} items</div>
+    )
+}
+
+export default Accordion;
---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Accordion from './Accordion'
+
+
+//create a couple of items to add to the accordion 
+const items = [
+    {
+            title: 'Is there a free trial available?', 
+            content: 'Sure! You can trial our copywriting tool for 14 days and if you like it, you can pay for a plan.'
+    },
+    {
+        title: 'Can I change my plan later?', 
+        content: 'Yep. To change your plan, go to your Account Settings, and then, to Billing. From here, you can choose a new plan.'
+    },
+    {
+        title: 'Can other information be added to my invoice?', 
+        content: 'Of course. From our Billing page, you can click on other bits of information like location and email address, which you can then add to your invoice.'
+    },
+    {
+        title: 'How does billing work?', 
+        content: 'We bill you based on the number of pages you use with our tool. You will be billed at the end of every month.'
+    }
+]
+
+
+
+const App = () => {
+    return (
+        <div>
+            <Accordion items={items} />
+        </div>
+    )
+}
+
+export default App;
