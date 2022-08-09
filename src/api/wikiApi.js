9fa4051618fe6c1d11b091d222b6e43922245ef3
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-08-09 07:10:22 +0400

    wired up axios for using the Wiki API

---
+++ src/api/wikiApi.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+//create an instance of the axios client with some properties
+export default axios.create({
+    baseURL: '',
+    params: {
+        
+    }
+})
--- src/components/SearchWidget.js
+++ src/components/SearchWidget.js
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const SearchWidget = () => {
     //initialize state
     const [term, setTerm ] = useState('')
-    const [results, setResults] = useState([])
+
+    //render when the component is run and the term has changed
+    useEffect(()=> {
+        console.log('rendering search results...');
+    }, [term])
+
 
     return (
-        <div>Here's a search component</div>
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter a search term</label>
+                    <input 
+                        value={term}
+                        onChange={ event => setTerm(event.target.value)}
+                        className="input" />
+                </div>
+            </div>
+        </div>
     )
 }
 
