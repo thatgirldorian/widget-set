64b12ad983f73b5b562bec851699094cae493cef
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-08-11 21:23:15 +0400

    added a constraint to the number of API requests

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,15 +4,27 @@ import axios from 'axios';
 //
 
 const Convert = ({ language, text}) => {
-    //add a piece of state to update the translated text
+    //add a piece of state to update the translated text & throttle requests
     const [translated, setTranslated] = useState('')
+    const [debouncedText, setDebouncedText] = useState(text)
+
+    //set this up for the timer
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text)
+        }, 700)
+
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [text])
 
     useEffect(() => {
          //create a helper function to update the translated text
     const doTheTranslation = async () => {
         const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, { 
             params: { 
-                q: text, 
+                q: debouncedText, 
                 target: language.value, 
                 key: 'AIzaSyDCXG1XmmdcpG8iTDVoJa-mzBlQLTTx5bo'
             }
@@ -24,7 +36,7 @@ const Convert = ({ language, text}) => {
     //call our translation function to be invoked when text/language is changed
     doTheTranslation()
 
-    }, [text, language])
+    }, [debouncedText, language])
 
 
     return (
