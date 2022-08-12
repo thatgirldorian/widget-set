c15c14c98bf4c319c80c3cd8cda3487f6fdf2454
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-08-12 08:09:34 +0400

    Added a way to change and update the URL

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,6 +5,8 @@ const Link = ({ className, href, children}) => {
     //add a helper function to prevent a full-page reload
     const onClick = (event) => {
         event.preventDefault()
+        //change the URL to match the content on screen
+        window.history.pushState({}, '', href)
     }
 
 
